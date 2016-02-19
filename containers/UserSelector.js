1cdd091554809af05d4cff386d4ce9a228f81396
Allan <allan.johnson1@me.com>
2016-02-19 15:06:42 -0800

    just updating local changes

--- containers/UserSelector.js
+++ containers/UserSelector.js
@@ -6,12 +6,13 @@ import { setUser } from '../actions'
 import Users from '../components/User'
 
 const mapStateToProps = (state) => {
-  return {
-    state.users
-  }
+  console.log("Map State To Props");
+  return
 };
 
 const mapDispatchToProps = (dispatch) => {
+  console.log("Map Dispatch To Props");
+
   return {
     onChange: (userId) => {
       dispatch(setUser(userId))
